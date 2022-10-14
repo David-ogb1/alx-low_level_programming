71285ec9a6db42e2e4e875ca2c320947a199c5c9
David-ogb1 <davidogbodo71@gmail.com>
2022-10-14 13:12:55 -0700

    my first code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: C programm that prints a functionfollowed by a new line
+ * Return: 0 if successful
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
