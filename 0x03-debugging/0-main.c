030d583c57d96606d0343e7699bbb9a4018653a2
David-ogb1 <davidogbodo71@gmail.com>
2022-10-21 15:44:39 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main- test function that prints if integer is post or neg
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	i= 0;
+	positive_or_negative(i);
+
+	return (0);
+}
